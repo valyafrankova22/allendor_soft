04ba2dd44d9fb9bb7896c5cdcc2fbe1de41678c2
valyafrankova22 <vfrankova22@icloud.com>
2022-11-01 13:45:12 +0200

    init

--- mail.php
+++ mail.php
@@ -1,54 +1,38 @@
 <?php 
 
-require 'phpmailer/PHPMailer.php';
-require 'phpmailer/SMTP.php';
-require 'phpmailer/Exception.php';
+require_once('PHPMailer-master/PHPMailerAutoload.php');
 
-$title = "Тема листа";
+$mail = new PHPMailer;
+$mail->CharSet = "UTF-8";
 
-$c = true;
-// Формирование самого письма
-$title = "Заголовок письма";
-foreach ( $_POST as $key => $value ) {
-  if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
-    $body .= "
-    " . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-      <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-      <td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-    </tr>
-    ";
-  }
-}
+$name = $_POST['name'];
+$phone = $_POST['tel'];
+$title = "заявка з сайта";
 
-$body = "<table style='width: 100%;'>$body</table>";
+$mail->isSMTP();
+$mail->SMTPAuth   = true;
+$mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+ $mail->Username   = 'allendorspace@gmail.com'; // Логин на почте
+ $mail->Password   = 'ovmeajkzbsgmwedy'; // Пароль на почте
+ $mail->SMTPSecure = 'ssl';
+ $mail->Port       = 465;
 
-$mail = new PHPMailer\PHPMailer\PHPMailer();
+$mail->setFrom('allendorspace@gmail.com', 'Заявка з сайта'); // Адрес самой почты и имя отправителя
+$mail->addAddress('allendorspace@gmail.com');
+$mail->addAddress('officepelicano@gmail.com');
 
-try {
-    $mail->isSMTP();
-    $mail->CharSet = "UTF-8";
-    $mail->SMTPAuth   = true;
-  
-    // Настройки вашей почты
-    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
-    $mail->Username   = 'allendorspace@gmail.com'; // Логин на почте
-    $mail->Password   = 'ovmeajkzbsgmwedy'; // Пароль на почте
-    $mail->SMTPSecure = 'ssl';
-    $mail->Port       = 465;
-  
-    $mail->setFrom('allendorspace@gmail.com', 'Заявка з сайта'); // Адрес самой почты и имя отправителя
-  
-    // Получатель письма
-    $mail->addAddress('allendorspace@gmail.com');
-    $mail->addAddress('officepelicano@gmail.com');
-
-    // Отправка сообщения
-  $mail->isHTML(true);
+$mail->isHTML(true);
   $mail->Subject = $title;
-  $mail->Body = $body;
+  $mail->Body = '' .$name . 'Залишив заявку' .$phone;
+  $mail->AltBody = '';
 
-  $mail->send();
+  if(!$mail->send()) {
+    echo 'Error';
+  } else {
+    echo 'Заявка надіслана';
+  }
 
-} catch (Exception $e) {
-  $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
-}
+
+
+
+
