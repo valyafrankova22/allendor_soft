e4a30c8866dd2776eb21e897b004d9ce94b02e0d
valyafrankova22 <vfrankova22@icloud.com>
2022-11-04 11:36:38 +0200

    init

--- js/index.js
+++ js/index.js
@@ -1,37 +1,100 @@
 'use strict'
 
+document.addEventListener(`DOMContentLoaded`, function () {
+    const form = document.getElementById('form');
+    form.addEventListener('submit', formSend);
+
+    console.log(form);
+    async function formSend (e) {
+        e.preventDefault();
+        let error = formValidate(form);
+        let formData = new FormData(form);
+
+        if (error === 0) {
+            $(document).ready(function() {
+
+                //E-mail Ajax Send
+                $("form").submit(function() { //Change
+                    var th = $(this);
+                    $.ajax({
+                        type: "POST",
+                        url: "mail.php", //Change
+                        data: th.serialize()
+                    }).done(function() {
+                        alert("Thank you!");
+                        setTimeout(function() {
+                            // Done Functions
+                            th.trigger("reset");
+                        }, 1000);
+                    });
+                    return false;
+                });
+
+            });
+        } else {
+            alert('Заповніть будь ласка всі поля)');
+        }
+
+    }
+
+    function formValidate (form) {
+            let error = 0;
+            let formReq = document.querySelectorAll('._req');
+
+            for (let index = 0; index < formReq.length; index++) {
+                 const input = formReq[index];
+                 formRemoveError(input);
+
+                 if(input.value === '') {
+                     formAddError(input);
+                     error++;
+                 }
+            }
+            return error
+        }
 
 
-jQuery(document).ready(function () {
-   
-  
-   jQuery('.form_button').click( function() {
-       var form = jQuery(this).closest('form');
-       
-       if ( form.valid() ) {
-           form.css('opacity','.5');
-           var actUrl = form.attr('action');
-
-           jQuery.ajax({
-               url: actUrl,
-               type: 'post',
-               dataType: 'html',
-               data: form.serialize(),
-               success: function(data) {
-                   form.html(data);
-                   form.css('opacity','1');
-                   //form.find('.status').html('форма отправлена успешно');
-                   //$('#myModal').modal('show') // для бутстрапа
-               },
-               error:	 function() {
-                    form.find('.status').html('серверная ошибка');
-               }
-           });
-       }
-   });
+    function formAddError (input) {
+        input.parentElement.classList.add('._error');
+        input.classList.add('._error')
+    }
+    function formRemoveError (input) {
+        input.parentElement.classList.remove('._error');
+        input.classList.remove('._error')
+    }
+})
 
 
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
